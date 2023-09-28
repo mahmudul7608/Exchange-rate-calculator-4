64d997f4e51e74dafbc4f7118f3bbaf9659306fb
Mahmudul Hasan <dev.mahmudulhasan2@gmail.com>
2023-09-29 03:24:49 +0600

    done this project

---
+++ img/script.js
@@ -0,0 +1,11 @@
+const currencyEl_one = document.getElementById('currency-one');
+
+const amountEl_one = document.getElementById('mount-one');
+
+const currencyEl_two = document.getElementById('currency-one');
+
+const currencyEl_two = document.getElementById('currency-one');
+
+function calculate(){}
+
+calculate();
